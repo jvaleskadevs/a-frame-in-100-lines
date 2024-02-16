0c1fb3b7ed504c18ed4af8c51fc19e38d6ff9775
jvaleskadevs <vmjune2019@gmail.com>
2024-02-16 02:30:57 +0100

    resultsFix

--- app/results/Plot.tsx
+++ app/results/Plot.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
-import Plot from 'react-plotly.js';
+//import React from 'react';
+//import Plot from 'react-plotly.js';
 
-interface IPlotProps {
-  imageId: string | number;
-  wowow: string | number;
-  meh: string | number;
-}
-
+/*
 export default function ResultPlot (props: IPlotProps) {
   const { imageId, wowow, meh } = props;
   return (
@@ -24,4 +19,64 @@ export default function ResultPlot (props: IPlotProps) {
       layout={ {width: 320, height: 240, title: `Image #${imageId}`} }
     />
   );
+}
+*/
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+interface IPlotProps {
+  imageId: string | number;
+  wowow: string | number;
+  meh: string | number;
+}
+
+export default function ResultPlot(props: IPlotProps) {
+  const { imageId, wowow, meh } = props;
+  const labels = ['wowow', 'meh'];
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: `Image #${imageId}`,
+        data: [wowow, meh],
+        backgroundColor: 'rgb(255, 99, 132)',
+      }
+    ],
+  };  
+  const options = {
+    plugins: {
+      title: {
+        display: true,
+        text: 'Bar Chart Results',
+      },
+    },
+    responsive: true,
+    scales: {
+      x: {
+        stacked: false,
+      },
+      y: {
+        stacked: false,
+      },
+    },
+  };
+  return <Bar options={options} data={data} />;
 }
