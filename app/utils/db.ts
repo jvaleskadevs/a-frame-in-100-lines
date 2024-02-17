2510a62484c8ee29509c20e5d624280e045e4ff8
jvaleskadevs <vmjune2019@gmail.com>
2024-02-17 02:25:30 +0100

    newDB

--- app/utils/db.ts
+++ app/utils/db.ts
@@ -40,9 +40,9 @@ function deleteAll() {
 
 
 function readData() {
-  return JSON.parse(fs.readFileSync('data/results.json', 'utf8'));
+  return JSON.parse(fs.readFileSync('../../data/results.json', 'utf8'));
 }
 
 function writeData(data: any) {
-  fs.writeFileSync('data/results.json', JSON.stringify(data, null, 4));
+  fs.writeFileSync('../../data/results.json', JSON.stringify(data, null, 4));
 }
