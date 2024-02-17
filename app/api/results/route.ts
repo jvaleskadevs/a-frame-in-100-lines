8042311e830e6506e28e481206c7b6d185050ab0
jvaleskadevs <vmjune2019@gmail.com>
2024-02-17 02:21:18 +0100

    newDB

--- app/api/results/route.ts
+++ app/api/results/route.ts
@@ -1,11 +1,14 @@
 import { kv } from '@vercel/kv';
 import { NextResponse } from 'next/server';
+import { resultsDB } from '../../utils/db';
  
 export async function GET() {
+  /*
   const maxId = 840;
   const results = [];
   for (let i = 1; i < maxId; i++) {
     results.push(await kv.hgetall(`dfbc:imageId:${i}`));
   }
-  return NextResponse.json(results);
+  */
+  return NextResponse.json(resultsDB.getAll());
 }
