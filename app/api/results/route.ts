0dd84e6b1906023ee1ff82b30688ac2fe892c0af
jvaleskadevs <vmjune2019@gmail.com>
2024-02-20 00:07:29 +0100

    showResults

--- app/api/results/route.ts
+++ app/api/results/route.ts
@@ -2,14 +2,14 @@ import { kv } from '@vercel/kv';
 import { NextResponse } from 'next/server';
  
 export async function GET() {
-  /*
+
   const maxId = 840;
   const results = [];  
   for (let i = 1; i < maxId; i++) {
-    results.push(await kv.hgetall(`dfbc:imageId:${i}`));
+    results.push(await kv.hgetall(`dfbc:${i}`));
   }
-  */
+
   return NextResponse.json({});
 }
 
-//export const dynamic = 'force-dynamic';
+export const dynamic = 'force-dynamic';
